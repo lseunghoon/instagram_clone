83b3e5aed01a16f63ffccfb4cb34acd5b763d022
seunghoonLee <zinozico0070@gmail.com>
2021-01-27 23:25:54 +0900

    첫번째 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('찐따 앱'),
+        ),
+        backgroundColor: Colors.yellow[200],
+      ),
+    );
+  }
+}
