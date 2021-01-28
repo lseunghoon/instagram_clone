1d6c6842cbab528249c8c5acf97fde52daf40333
seunghoonLee <zinozico0070@gmail.com>
2021-01-28 22:03:03 +0900

    아바타 위젯 만들기 전 커밋

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:instagram_clone/widgets/common_size.dart';
+import 'package:instagram_clone/widgets/my_progress_indicator.dart';
 
 class Post extends StatelessWidget {
   final int index;
+  Size size;
 
   Post(this.index);
 
   @override
   Widget build(BuildContext context) {
+    if (size == null) {
+      size = MediaQuery.of(context).size;
+    }
+
+    return Column(
+      children: [
+        _postHeader(),
+        _postImage(),
+      ],
+    );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: ClipOval(
+            child: CachedNetworkImage(
+              imageUrl: 'https://picsum.photos/100',
+              height: avatar_size,
+              width: avatar_size,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Text('username'),
+        ),
+        IconButton(
+            icon: Icon(
+              Icons.more_horiz,
+              color: Colors.black87,
+            ),
+            onPressed: null),
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
     return CachedNetworkImage(
-      imageUrl: 'https://picsum.photos/id/$index/200/200',
+      imageUrl: 'https://picsum.photos/id/$index/2000/2000',
       imageBuilder: (BuildContext context, ImageProvider imageProvider) {
         return AspectRatio(
           aspectRatio: 1,
@@ -18,6 +60,12 @@ class Post extends StatelessWidget {
               image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
             ),
           ),
+        );
+      },
+      placeholder: (BuildContext context, String url) {
+        return MyProgressIndicator(
+          containerSize: size.width,
+          progressSize: 50,
         );
       },
     );
