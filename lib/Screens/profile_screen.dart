987a3af0b0d13eeefe0b2af6196d04bdbe421120
seunghoonLee <zinozico0070@gmail.com>
2021-01-31 00:25:50 +0900

    프로필 바디 온메뉴체인지드

--- lib/Screens/profile_screen.dart
+++ lib/Screens/profile_screen.dart
@@ -3,19 +3,28 @@ import 'package:instagram_clone/widgets/profile_body.dart';
 import 'package:instagram_clone/constants/screen_size.dart';
 
 class ProfileScreen extends StatelessWidget {
+  final duration = Duration(milliseconds: 300);
+  MenuStatus _menuStatus = MenuStatus.closed;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: Stack(
         children: <Widget>[
-          ProfileBody(),
-          Positioned(
-            top: 0,
-            bottom: 0,
-            width: size.width / 2,
-            child: Container(
-              color: Colors.deepPurpleAccent,
+          AnimatedContainer(
+            duration: duration,
+            child: ProfileBody(onMenuChanged: () {}),
+          ),
+          AnimatedContainer(
+            duration: duration,
+            child: Positioned(
+              top: 0,
+              bottom: 0,
+              width: size.width / 2,
+              child: Container(
+                color: Colors.deepPurpleAccent,
+              ),
             ),
           ),
         ],
@@ -23,3 +32,5 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 }
+
+enum MenuStatus { opened, closed }
