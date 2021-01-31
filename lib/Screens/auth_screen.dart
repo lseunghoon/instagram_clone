e2a5abb6f621f429f14e8d795275e71fbd687001
seunghoonLee <zinozico0070@gmail.com>
2021-01-31 21:54:50 +0900

    Sign up page +

--- lib/Screens/auth_screen.dart
+++ lib/Screens/auth_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/Screens/profile_screen.dart';
 import 'package:instagram_clone/widgets/fade_stack.dart';
-import 'package:instagram_clone/widgets/sign_in_form.dart';
-import 'package:instagram_clone/widgets/sign_up_form.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -21,20 +18,44 @@ class _AuthScreenState extends State<AuthScreen> {
             FadeStack(
               selectedForm: selectedForm,
             ),
-            Container(
-              child: FlatButton(
-                child: Text('go to Sign up'),
-                onPressed: () {
-                  setState(
-                    () {
-                      if (selectedForm == 0) {
-                        selectedForm = 1;
-                      } else {
-                        selectedForm = 0;
-                      }
-                    },
-                  );
-                },
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              height: 40,
+              child: Container(
+                color: Colors.white,
+                child: FlatButton(
+                  shape: Border(
+                    top: BorderSide(color: Colors.grey),
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      text: selectedForm == 0
+                          ? 'Already have an account? '
+                          : 'Don\'t have an account? ',
+                      style: TextStyle(color: Colors.grey),
+                      children: [
+                        TextSpan(
+                          text: selectedForm == 0 ? 'Sign In' : 'Sign Up',
+                          style: TextStyle(
+                              color: Colors.blue, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(
+                      () {
+                        if (selectedForm == 0) {
+                          selectedForm = 1;
+                        } else {
+                          selectedForm = 0;
+                        }
+                      },
+                    );
+                  },
+                ),
               ),
             ),
           ],
--- lib/widgets/sign_up_form.dart
+++ lib/widgets/sign_up_form.dart
@@ -1,10 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/widgets/common_size.dart';
 
-class SignUpForm extends StatelessWidget {
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _pwController = TextEditingController();
+  TextEditingController _cpwController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _cpwController.dispose();
+    _pwController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepOrangeAccent,
+    return Padding(
+      padding: const EdgeInsets.all(common_gap),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: common_l_gap,
+            ),
+            Image.asset('assets/images/insta_text_logo.png'),
+            TextFormField(
+              validator: (text) {
+                if (text.isNotEmpty && text.contains('@')) {
+                  return null;
+                } else {
+                  return '정확한 이메일 주소를 입력해주세요';
+                }
+              },
+              controller: _emailController,
+              decoration: _inputTextDeco('Email'),
+            ),
+            TextFormField(
+              validator: (text) {
+                if (text.isNotEmpty && text.length > 5) {
+                  return null;
+                } else {
+                  return '비밀번호는 다섯자리 이상이어야 합니다';
+                }
+              },
+              controller: _pwController,
+              decoration: _inputTextDeco('Password'),
+            ),
+            TextFormField(
+              validator: (text) {
+                if (text.isNotEmpty && _pwController.text == text) {
+                  return null;
+                } else {
+                  return '비밀번호와 비밀번호 확인 값이 일치하지 않습니다';
+                }
+              },
+              controller: _cpwController,
+              decoration: _inputTextDeco('Confirm Password'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration _inputTextDeco(String hint) {
+    return InputDecoration(
+      filled: true,
+      fillColor: Colors.grey[100],
+      hintText: hint,
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(common_s_gap),
+        borderSide: BorderSide(color: Colors.grey[300]),
+      ),
     );
   }
 }
